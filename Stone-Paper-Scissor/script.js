73f1683bb02b086ed0ebcd2eac3534ac6f4119ee
Yash Gohel <115644973+engineer-yash@users.noreply.github.com>
2023-03-19 09:17:38 +0530

    Update script.js

--- Stone-Paper-Scissor/script.js
+++ Stone-Paper-Scissor/script.js
@@ -17,26 +17,31 @@ function match(user, cpu) {
 }
 
 function myFunction() {
-  document.querySelector("#div").style.display = "none";
-  document.querySelector("#output").style.display = "";
+  let a = document.querySelector("#div").style.display = "none";
   let cpuI = Math.floor(Math.random() * 3);
   let cpu = ["stone", "paper", "scissor"][cpuI];
   let user = "";
   let radios = document.getElementsByName("rad");
- 
-  
+  let c = document.querySelector("#output").style.display = "";
+
   for (i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       user = radios[i].value;
-      
+
     }
   }
- 
+
   let result = match(user, cpu);
-  document.querySelector("#output").innerHTML = `USER: ${user.toUpperCase()}<br>CPU: ${cpu.toUpperCase()}<br><br><h3 style="color:green"> The Winner is: ${result.toUpperCase()}</h3><br><input class="button" type="submit" value="Try Again!" onclick="tryAgain()">`;
+  document.querySelector("#output").innerHTML = `USER: ${user.toUpperCase()}<br><br>CPU: ${cpu.toUpperCase()}<br><br>
+  <h3 style="color:green; border:2px dashed; padding:5px;">
+  The Winner is: ${result.toUpperCase()}
+  </h3>
+  <br><input class="button" type="submit" value="Try Again!" onclick="tryAgain()">`;
+  
 }
 
-function tryAgain(){
-  document.querySelector("#output").style.display = "none";
-  document.querySelector("#div").style.display = "";
+
+function tryAgain() {
+  let c = document.querySelector("#output").style.display = "none";
+  let a = document.querySelector("#div").style.display = "";
 }
